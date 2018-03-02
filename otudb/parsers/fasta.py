0f1be314c19082504abbac22f9336d8df1db09d4
cometsong <benjamin@cometsong.net>
2018-03-02 12:42:15 -0500

    parser fasta: mod call to write_seq using bases as str or list

--- otudb/parsers/fasta.py
+++ otudb/parsers/fasta.py
@@ -13,7 +13,7 @@ log = log_it(logname='otudb.parser')
 
 @attr.s
 class FastaParser(TextParser):
-    """Wrapper methods for 'csv' package"""
+    """Wrapper methods for 'FASTA' files"""
     lead_character: str = attr.ib(default='>')
     line_width: int = attr.ib(default='80')
 
@@ -32,9 +32,6 @@ class FastaParser(TextParser):
                 line = group.next()
                 header_value = group.next()[1:].strip()
             else:
-                #TODO: check in .groupby if group is list or generator
-                # self.line_width = len(group[0])
-
                 sequence = ''.join(line.strip() for line in group)
                 yield header_value, sequence
 
@@ -47,17 +44,21 @@ class FastaParser(TextParser):
             raise e
 
 
-    def write_seq(self, bases, delimiter=None):
-        """write all sequence bases to filename"""
+    def write_seq(self, bases):
+        """write all sequence bases to filename
+        'bases' can str or list
+        """
         seqs = []
         if not bases:
             log.exception(f'Missing bases for {self.filename}')
             return False
-        # elif isinstance(bases, list):
-        else:
+        elif isinstance(bases, str):
             while bases:
                 seqs.append(bases[0:self.line_width-1])
                 bases = bases[self.line_width:]
+        elif isinstance(bases, list):
+            seqs = bases
+        else:
 
         return self.write(self, headers=None, values=[seqs], delimiter=None)
 
